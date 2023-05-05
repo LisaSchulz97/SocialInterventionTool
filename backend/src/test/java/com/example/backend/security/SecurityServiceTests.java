31503202fdb6a3156595c440e183fa9720290c1e
LisaBagszis <lisa.bagszis@gmail.com>
2023-05-05 15:54:38 +0200

    finished all Tests (integration and service) for question and questionnaire

--- backend/src/test/java/com/example/backend/security/SecurityServiceTests.java
+++ backend/src/test/java/com/example/backend/security/SecurityServiceTests.java
@@ -3,9 +3,7 @@ package com.example.backend.security;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
 import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchException;
 import static org.mockito.Mockito.*;
